9438f297a1f570183e15b4c8df924b67e6623d84
PKMNMASTER33OGUZHAN <100959364+PKMNMASTER33OGUZHAN@users.noreply.github.com>
2022-11-05 22:33:09 +0300

    Add files via upload

---
+++ sayı alma.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	int i;
+	printf("5 ten buyuk sayi gir");
+	scanf("%d",&i);
+	if(i>5)	
+	{
+		printf("aferim");
+    }
+    else 
+    {
+	printf("cok ayip");
+	
+	}
+	
+
+}
